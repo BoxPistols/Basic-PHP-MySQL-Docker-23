9f239ec601bc2cef5af76328335b152723f1bde5
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-01-07 07:38:33 +0900

    Add form and update PHP code

--- src/index.php
+++ src/index.php
@@ -5,67 +5,75 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/asagiri@0.1.1/css/main.min.css">
   <link rel="stylesheet" href="/style.css" />
 </head>
 
 <body>
   <h1>The PHP</h1>
   <main>
-  <?php echo "Hello!" . "<br>"; ?>
 
-  <?php
-  date_default_timezone_set('Asia/Tokyo');
-  // hour
-  // echo date('H') . "<br>";
-  echo ("Now:") . date('Y/m/d H:i:s') . "<br>";
-  ?>
-  
-  <hr>
-  
-  <?php
-  // $i = mt_rand(1, 6);
-  $i = mt_rand() % 100;
+    <h2>Form</h2>
+    <form action="index.php" method="POST">
+      <label>名前: <input type="text" name="target_name"></label>
+      <input type="submit" value="送信">
+    </form>
+    <!-- target_name -->
+    <?php
+    date_default_timezone_set('Asia/Tokyo');
+    // echo date('H') . "<br>";
+    echo ("Now:") . date('Y/m/d H:i:s') . "<br>";
+    echo "Hello, " . $_POST['target_name'] . "!";
+    ?>
 
-  var_dump($i);
-  echo "<br>";
+    <hr>
 
-  ++$i;
-  var_dump($i);
-  echo "<br>";
+    <h2>Basic</h2>
 
-  $i++;
-  var_dump($i);
-  echo "<br>";
+    <?php
+    // $i = mt_rand(1, 6);
+    $i = mt_rand() % 100;
 
-  echo '$i';
+    var_dump($i);
+    echo "<br>";
 
-  echo "<br>";
-  // unset($i);
-  $i = "日本語?";
-  var_dump($i);
-  echo "<br>";
+    ++$i;
+    var_dump($i);
+    echo "<br>";
 
-  echo "<hr>";
-  ?>
-  <!-- 関数 -->
-  <?php
-  function checkNumber($number)
-  {
-    if ($number > 1) {
-      throw new Exception('The number is greater than 1');
+    $i++;
+    var_dump($i);
+    echo "<br>";
+
+    echo '$i';
+
+    echo "<br>";
+    // unset($i);
+    $i = "日本語?";
+    var_dump($i);
+    echo "<br>";
+
+    echo "<hr>";
+    ?>
+    <!-- 関数 -->
+    <?php
+    function checkNumber($number)
+    {
+      if ($number > 1) {
+        throw new Exception('The number is greater than 1');
+      }
     }
-  }
-  // Try Catch block
-  try {
-    checkNumber(10);
-  } catch (Exception $e) {
-    echo $e->getMessage();
-  }
-  ?>
+    // Try Catch block
+    try {
+      checkNumber(10);
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+    ?>
 
-  <?
-  /* php phpinfo(); */
-  ?>
+    <?
+    /* php phpinfo(); */
+    ?>
   </main>
 </body>
 
